e498a135dabb5227804af9bba1822d03c6f6ffcd
Jules Bousrez <38622433+julesbsz@users.noreply.github.com>
2023-06-17 23:52:22 -0400

    fix: city research + improvements

--- exo1_python/api_handler.py
+++ exo1_python/api_handler.py
@@ -1,50 +1,67 @@
 # Récupération des données de l'API JCDecaux
 
-import requests  # Nécessaire pour faire des requêtes HTTP
-# Importation des variables de config
 from config import API_BASE_URL, API_KEY, API_VERSION
-# Nécessaire pour afficher les informations sous forme de tableau
+
+import requests  # Librairie pour envoyer des requêtes HTTP
+# Librairie pour afficher des tableaux dans le terminal
 from tabulate import tabulate
 
-# Récupérer les stations
 
-
-def get_stations():
-    url = f"{API_BASE_URL}/{API_VERSION}/stations?apiKey={API_KEY}"
+def get_stations():  # Récupérer les stations
+    url = f"{API_BASE_URL}/vls/{API_VERSION}/stations?apiKey={API_KEY}"
     response = requests.get(url)
-    print(url)
     return response.json()
 
-# Initialiser les informations
+
+# Récupérer une station (ne fonctionne pas)
+def get_single_station(station_number, contract_name):
+    url = f"{API_BASE_URL}/vls/{API_VERSION}/stations/{station_number}?contract={contract_name}?apiKey={API_KEY}"
+    print(url)
+    response = requests.get(url)
+    return response.json()  # Response: {error: 'unauthorized'}
 
 
-def get_cities():
+def get_contracts():  # Récupérer les contrats (ne fonctionne pas)
+    url = f"{API_BASE_URL}/vls/{API_VERSION}/contracts?apiKey={API_KEY}"
+    print(url)
+    response = requests.get(url)
+    return response.json()  # Response: {error: 'unauthorized'}
+
+
+# Récupérer les parcs d'un contrat (ne fonctionne pas)
+def get_park_list_of_contract(contract_name):
+    url = f"{API_BASE_URL}/parking/{API_VERSION}/contracts/{contract_name}/parks?apiKey={API_KEY}"
+    print(url)
+    response = requests.get(url)
+    return response.json()  # Response: {error: 'unauthorized'}
+
+
+def get_cities():  # Récupérer les villes
     stations = get_stations()
     cities = list(set([station["contract_name"] for station in stations]))
     cities.sort()
     return cities
 
 
-def get_info():
+def get_global_info():  # Récupérer les informations globales (action 1 du menu principal)
     stations = get_stations()
-    nb_stations = len(stations)
-    nb_bikes = sum([station["bike_stands"] for station in stations])
 
-    # Ajout des informations demandées
-    nb_open_stations = sum(
+    nb_stations = len(stations)
+    nb_stations_open = sum(
         [1 for station in stations if station["status"] == "OPEN"])
-    nb_closed_stations = nb_stations - nb_open_stations
+    nb_stations_closed = nb_stations - nb_stations_open
+
     nb_available_bikes = sum([station["available_bikes"]
                              for station in stations])
-    nb_unavailable_bikes = nb_bikes - nb_available_bikes
+    nb_available_bike_stands = sum(
+        [station["available_bike_stands"] for station in stations])
 
     info = {
         "Nombre de stations": nb_stations,
-        "Stations ouvertes": nb_open_stations,
-        "Stations fermées": nb_closed_stations,
-        "Nombre de vélos": nb_bikes,
-        "Vélos disponibles": nb_available_bikes,
-        "Vélos indisponibles": nb_unavailable_bikes,
+        "Stations ouvertes": nb_stations_open,
+        "Stations fermées": nb_stations_closed,
+        "Nombre de vélos": nb_available_bikes,
+        "Nombre de places": nb_available_bike_stands,
     }
 
     table = [[key, value] for key, value in info.items()]
@@ -52,41 +69,52 @@ def get_info():
                                     "Valeur"], tablefmt="grid")
 
 
+# Récupérer les informations par ville (action 2 du menu principal)
 def get_info_by_city(city):
     stations = get_stations()
     cities = get_cities()
 
+    # Si la ville n'est pas renseignée, on retourne un statut d'erreur avec la liste des villes disponibles
     if (city == "" or city == " "):
+        available_cities = ", ".join(cities)
+        response = [{"status": "error"}, {
+            "message": f"Villes disponibles : {available_cities}"}]
+        return response
 
-        return f"Villes disponibles : {', '.join(cities)}"
-
+    # On met la ville entrée en minuscule
     city = city.lower()
 
-    stations_by_city = [
-        station for station in stations if station["contract_name"] == city]
-    nb_stations = len(stations_by_city)
-    nb_bikes = sum([station["bike_stands"] for station in stations_by_city])
+    # Si la ville n'existe pas, on retourne un statut d'erreur avec un message d'erreur
+    if city not in cities:
+        response = [{"status": "error"},
+                    {"message": f"La ville {city} n'existe pas. Veuillez réessayer."}]
+        return response
 
-    if (nb_stations == 0):
-        return f"Aucune station n'a été trouvée pour la ville de {city}"
+    # On récupère les stations de la ville
+    stations = [
+        station for station in stations if station["contract_name"].lower() == city]
 
-    # Ajout des informations demandées
-    nb_open_stations = sum(
-        [1 for station in stations_by_city if station["status"] == "OPEN"])
-    nb_closed_stations = nb_stations - nb_open_stations
+    nb_stations = len(stations)
+    nb_stations_open = sum(
+        [1 for station in stations if station["status"] == "OPEN"])
+    nb_stations_closed = nb_stations - nb_stations_open
+
     nb_available_bikes = sum([station["available_bikes"]
-                             for station in stations_by_city])
-    nb_unavailable_bikes = nb_bikes - nb_available_bikes
+                              for station in stations])
+    nb_available_bike_stands = sum(
+        [station["available_bike_stands"] for station in stations])
 
     info = {
         "Nombre de stations": nb_stations,
-        "Stations ouvertes": nb_open_stations,
-        "Stations fermées": nb_closed_stations,
-        "Nombre de vélos": nb_bikes,
-        "Vélos disponibles": nb_available_bikes,
-        "Vélos indisponibles": nb_unavailable_bikes,
+        "Stations ouvertes": nb_stations_open,
+        "Stations fermées": nb_stations_closed,
+        "Nombre de vélos": nb_available_bikes,
+        "Nombre de places": nb_available_bike_stands,
     }
 
+    # On retourne un statut de succès avec les informations sous forme de tableau
     table = [[key, value] for key, value in info.items()]
-    return tabulate(table, headers=["Information",
-                                    "Valeur"], tablefmt="grid")
+    message = tabulate(table, headers=["Information",
+                                       "Valeur"], tablefmt="grid")
+    response = [{"status": "success"}, {"message": message}]
+    return response
--- exo1_python/main.py
+++ exo1_python/main.py
@@ -1,40 +1,67 @@
 # Affichage des données
 
 from rich import print  # Librairie pour améliorer l'affichage dans le terminal
-from api_handler import get_info, get_info_by_city, get_cities
+from api_handler import get_global_info, get_info_by_city
 
-welcomeMessage = '''
+mainMenu = '''
 
 [bold blue]Bienvenue sur l'application de gestion des vélos en libre-service[/bold blue]
 
 [bold]Menu principal[/bold]
 1. Informations générales
-2. Information par ville
+2. Informations par ville
 
-3. Afficher le menu
-4. Quitter
+3. Quitter
 
 '''
 
-print(welcomeMessage)
+# Lancer le programme
 
-action = input('Entrer un nombre pour commencer : ')
 
-while action != '4':
-    print("\n")
+def run(mainMenu):
+    print("\n" * 100 + mainMenu)
 
-    if action == '1':
-        print(get_info(), end='\n\n')
-    elif action == '2':
-        city = input(
-            'Entrer le nom de la ville (laisser vide pour afficher la liste des villes disponibles): ')
-        print(get_info_by_city(city), end='\n\n')
-    elif action == '3':
-        print('\n' * 100)
-        print(welcomeMessage)
-    else:
-        print('Action inconnue')
+    action = input('Entrer un nombre pour commencer : ')
 
-    action = input('Entrer un nombre pour continuer : ')
+    # Tant que l'action n'est pas 3 (quitter) -> on continue
+    while action != '3':
+        print("\n")
 
-print('À bientôt !', end='\n\n')
+        # Si l'action est 1, on affiche les informations globales
+        if action == '1':
+            print(get_global_info(), end='\n\n')
+
+        # Si l'action est 2, on demande une ville à l'utilisateur -> on affiche les informations de cette ville
+        elif action == '2':
+            city = input(
+                'Entrer le nom de la ville (laisser vide pour afficher la liste des villes disponibles): ')
+            response = get_info_by_city(city)
+
+            # Tant que le statut de la réponse est une erreur, on affiche le message d'erreur et on redemande une ville à l'utilisateur
+            while response[0]["status"] == "error":
+                print("[bold red]" + response[1]
+                      ["message"] + "[/bold red]", end='\n\n')
+                city = input(
+                    'Entrer le nom de la ville (laisser vide pour afficher la liste des villes disponibles): ')
+                response = get_info_by_city(city)
+
+            # Si la ville entrée existe, on affiche les informations de cette ville
+            print('\n', response[1]["message"], end='\n\n')
+
+        # Si l'action est vide, on affiche le menu principal
+        elif action == '':
+            print('\n' * 100)
+            print(mainMenu)
+
+        # Si l'action n'existe pas, on affiche un message d'erreur
+        else:
+            print(
+                '[bold red]Action inconnue, veuillez réessayer.[/bold red]', end='\n\n')
+
+        action = input(
+            'Entrer un nombre pour continuer (laisser vide pour afficher au menu principal): ')
+
+    print('À bientôt !', end='\n\n')
+
+
+run(mainMenu)
